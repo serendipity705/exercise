c0b38945f52f8f3a4fbcfae015014daaaba04ef6
纸君 <101634494+serendipity705@users.noreply.github.com>
2022-07-03 16:31:12 +0800

    字符串排序
    本题要求编写程序，读入5个字符串，按由小到大的顺序输出。
    
    输入格式：
    输入为由空格分隔的5个非空字符串，每个字符串不包括空格、制表符、换行符等空白字符，长度小于80。
    
    输出格式：
    按照以下格式输出排序后的结果：
    
    After sorted:
    每行一个字符串

---
+++ StringSorting.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+	char str[5][80];
+	for ( int i=0; i<5; i++) {
+		scanf("%s",str[i]);
+	}
+	for ( int i=0; i<4; i++) {
+		for ( int j=i+1; j<5; j++) {
+			if( strcmp(str[i], str[j]) > 0 ) {
+				char temp[81];
+				strcpy(temp, str[i]);
+				strcpy(str[i], str[j]);
+				strcpy(str[j], temp);
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		printf("%s\n", str[i]);
+	}
+	return 0;
+}
